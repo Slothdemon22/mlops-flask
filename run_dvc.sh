b1ddb753f201276717ee6234a218882b30c1e63b
Slothdemon22 <www.basilslothdemon.com>
2025-12-10 23:25:51 +0500

    CICD pipeline 1

--- run_dvc.sh
+++ run_dvc.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 # Activate the virtual environment
-source /root/mlops-flask/venv/bin/activate
 
-# Navigate to project directory
-cd /root/mlops-flask
 
 # Run the DVC pipeline
 dvc repro
